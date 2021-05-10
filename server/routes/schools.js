c2c608684021d815080f682ca048480f7310e395
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-05-10 14:54:09 +0100

    docker build

--- server/routes/schools.js
+++ server/routes/schools.js
@@ -11,4 +11,24 @@ router.get('/', (req, res) => {
     res.json({status: "ok"})
 })
 
+router.get('/:id', async (req, res) => {
+    _id = req.params.id
+    db.get(
+        `SELECT * FROM Schools WHERE id = ${_id};`
+    , (error, data) => {
+        if (error) {
+            console.error(error);
+            return res.status(404).json({
+                status: 404,
+                message: `http://localhost/schools/${_id} has not found a school!`
+            })
+        } else {
+            return res.status(201).json({
+                status: 201,
+                data: data
+            })
+        }
+    })
+})
+
 module.exports = router;
